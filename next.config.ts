f3917a450115e61736aeea41001eaba79499d753
Techsoul437 <info@techsoulsolutions.com>
2026-06-25 15:57:38 +0530

    slove map issue

--- next.config.ts
+++ next.config.ts
@@ -33,31 +33,43 @@ const nextConfig = {
             key: "Permissions-Policy",
             value: "camera=(), microphone=(), geolocation=()",
           },
-         {
-  key: "Content-Security-Policy",
-  value: `
+          {
+            key: "Content-Security-Policy",
+            value: `
     default-src 'self';
+
     script-src 'self' 'unsafe-inline'
       https://www.googletagmanager.com
       https://www.google-analytics.com;
+
     style-src 'self' 'unsafe-inline';
+
     img-src 'self' data: blob:
       https://api.qrserver.com
       https://heartview-images.s3.ap-south-1.amazonaws.com
       https://www.google-analytics.com
-      https://www.googletagmanager.com;
+      https://www.googletagmanager.com
+      https://*.google.com
+      https://*.gstatic.com;
+
     connect-src 'self'
       https://www.google-analytics.com
       https://www.googletagmanager.com;
+
     font-src 'self' data:;
+
+    frame-src
+      https://www.google.com
+      https://www.google.com/maps;
+
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
   `
-    .replace(/\n/g, "")
-    .replace(/\s{2,}/g, " "),
-},
+              .replace(/\n/g, "")
+              .replace(/\s{2,}/g, " "),
+          },
         ],
       },
     ];
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -11,7 +11,7 @@ const categoryLabels: Record<string, string> = {
   bp: "Blood Pressure",
 
 };
-  import type { Metadata } from "next";
+import type { Metadata } from "next";
 
 //   export const  metadata: Metadata = {
 //   title: "Health Blog | HeartView Health",
